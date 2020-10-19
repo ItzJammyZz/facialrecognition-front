4fd5bd2b7fa0421de623c447b3b7c0dd5bc6bdbc
ItzJammyZz <jammy-anwar@hotmail.co.uk>
2020-10-19 15:08:21 +0100

    Updates

---
+++ precache-manifest.99d2c186fd4615ba3e0534ba358683be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636ecd22c8a169a56815656271c2f9ee",
+    "url": "/facialrecognition-front/index.html"
+  },
+  {
+    "revision": "ec51d2fe16fae620bc00",
+    "url": "/facialrecognition-front/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2dd0f31daae8e5884ec3",
+    "url": "/facialrecognition-front/static/css/main.2a6d4306.chunk.css"
+  },
+  {
+    "revision": "ec51d2fe16fae620bc00",
+    "url": "/facialrecognition-front/static/js/2.27adadbe.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/facialrecognition-front/static/js/2.27adadbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd0f31daae8e5884ec3",
+    "url": "/facialrecognition-front/static/js/main.8d71e073.chunk.js"
+  },
+  {
+    "revision": "158299f2e0e3acfcff3c",
+    "url": "/facialrecognition-front/static/js/runtime-main.9c8565a7.js"
+  }
+]);
