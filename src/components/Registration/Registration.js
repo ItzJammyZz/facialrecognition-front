4564369283626f8e5eba31f13cca74fe1fed2d7c
ItzJammyZz <jammy-anwar@hotmail.co.uk>
2023-10-27 18:46:45 +0100

    Console Log and catch error

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -34,11 +34,12 @@ class Registration extends React.Component {
     })
     .then(response => response.json())
     .then(user => {
+      console.log(user)
       if(user.id) {
         this.props.loadUser(user)
         this.props.onRouteChange('home');
       }
-    })
+    }) .catch((error) => console.log(error))
   }
   
   render() {
